e2a41a5e8b9074f07ef30b4302ab689a6516cb56
nandorfn <firnandorfn@gmail.com>
2024-08-10 23:24:33 +0700

    init

---
+++ app/(pages)/login/page.tsx
@@ -0,0 +1,10 @@
+
+const page: React.FC = () => {
+    return (
+        <>
+        <p>hello</p>
+        </>
+    );
+};
+
+export default page;
---
+++ app/provider/ApolloWrapper.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { ApolloLink, HttpLink } from "@apollo/client";
+import {
+  ApolloNextAppProvider,
+  ApolloClient,
+  InMemoryCache,
+  SSRMultipartLink,
+} from "@apollo/experimental-nextjs-app-support";
+
+function makeClient() {
+  const httpLink = new HttpLink({
+    uri: "/api/graphql",
+    fetchOptions: { cache: "no-store" },
+  });
+
+  return new ApolloClient({
+    cache: new InMemoryCache(),
+    link: httpLink,
+  });
+}
+
+export function ApolloWrapper({ children }: React.PropsWithChildren) {
+  return (
+    <ApolloNextAppProvider makeClient={makeClient}>
+      {children}
+    </ApolloNextAppProvider>
+  );
+}
