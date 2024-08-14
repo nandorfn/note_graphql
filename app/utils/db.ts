3dfaa88c13e3362a78acb2cd8feb2bfa24fa8f53
nandorfn <firnandorfn@gmail.com>
2024-08-14 19:57:50 +0700

    update connection db

--- app/utils/db.ts
+++ app/utils/db.ts
@@ -1,9 +1,13 @@
 import { Pool } from 'pg';
 
+// export const pool = new Pool({
+//   user: process.env.DB_USER,
+//   host: process.env.DB_HOST,
+//   database: process.env.DB_NAME,
+//   password: process.env.DB_PASSWORD,
+//   port: process.env.DB_PORT as number | undefined,
+// });
+
 export const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT as number | undefined,
+  connectionString: process.env.DATABASE_URL,
 });
